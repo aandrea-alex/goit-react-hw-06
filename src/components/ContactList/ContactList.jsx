945e6d5c51527ea7c7a370cb4fb256bf55529411
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-10-06 14:42:14 +0200

    added filter

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,25 @@
 import { useSelector } from 'react-redux';
 import { getItems } from '../../redux/selectors';
+import { selectNameFilter } from '../../redux/selectors';
 import styles from './ContactList.module.css';
 import ContactItem from '../ContactItem/ContactItem';
 
 const ContactList = () => {
   const contacts = useSelector(getItems);
+  console.log(contacts);
+  const nameFilter = useSelector(selectNameFilter);
+
+  const filteredContacts =
+  nameFilter?.length === 0
+      ? contacts
+      : contacts.filter(contact =>
+          contact.name.toLowerCase().includes(nameFilter.trim().toLowerCase())
+        );
 
   if (contacts.length === 0) return;
   return (
     <ul className={styles.contact}>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <li className={styles.item} key={contact.id}>
           <ContactItem contact={contact} />
         </li>
